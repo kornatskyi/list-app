caf23381bf1b5528fe97b3d822c78545f7210d1d
Bohdan <bohdan.kornatskyi@gmail.com>
2021-11-04 20:57:58 -0700

    adding post to the list of posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import InputForm from "./components/InputForm.jsx";
+import "./App.css";
+import Post from "./components/Post";
+import PostsContainer from "./components/PostsContainer.jsx";
+import { useState } from 'react'
+const data = [
+  {
+    name: "John Snow",
+    text: "Hello world!",
+
+
+  },
+  {
+    name: "Gandalf White",
+    text: "You shall not pass!!!"
+  },
+  {
+    name: "Lorem Impus",
+    text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking"
+  }
+]
 
 function App() {
+
+  const [posts, setPosts] = useState(data.map((obj, i) => <Post key={i} data={obj} />))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full flex flex-col">
+      <InputForm setPosts={setPosts} />
+      <PostsContainer>
+        {posts}
+      </PostsContainer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors')
 // tailwind.config.js
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {
     extend: {},
